f32279402f5bfbd2f3aa374b6fb4f0ddeb1fe2e1
vidya sagar <dvsagar2000@gmail.com>
2023-01-10 23:17:42 +0530

    update

--- Day 4/prob2.cpp
+++ Day 4/prob2.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-void smallest_subarray(std::vector<long long int> &a, int n, long long int p,
-                       int &start_index, int &count) {
-  int start = 0, end = 0, temp_count = 0;
+long long int smallest_subarray(std::vector<long long int> &a, long long int n, long long int p,
+                       long long int start_index, long long int &count) {
+  long long int start = 0, end = 0, temp_count = 0;
   long long int sum = 0;
-  int ans = INT_MAX;
+  long long int ans = INT_MAX;
   while (end < a.size()) {
     sum += a[end];
     while (sum >= p && start <= end) {
@@ -18,25 +18,31 @@ void smallest_subarray(std::vector<long long int> &a, int n, long long int p,
     end++;
   }
   count += temp_count;
+  return start_index;
 }
 int main() {
-  int n;
+  long long int n;
   long long int p;
   std::cin >> n >> p;
   std::vector<long long int> a(n, 0);
-  for (int i = 0; i < n; i++)
+  for (long long int i = 0; i < n; i++)
     std::cin >> a[i];
 
   long long int sum = 0;
   sum = std::accumulate(a.begin(), a.end(), sum);
   // std::cout << "sum:= " << sum << "\n";
-  int start_index = -1, count = 0;
+  long long int start_index = 0, count = 0;
   if (p >= sum) {
     count += (p / sum) * n;
-    p = p % sum;
+    p = p - ((p/sum)*sum);
+    start_index = 0;
   }
-  for (int i = 0; i < n - 1; i++)
+  if(p>0)
+  {
+    for (long long int i = 0; i < n - 1; i++)
     a.push_back(a[i]);
-  smallest_subarray(a, n, p, start_index, count);
+    long long int k = smallest_subarray(a, n, p, -1, count);
+    start_index = k;
+  }
   std::cout << start_index + 1 << " " << count << "\n";
 }
